919b9cf98d2d4a16ad0c02e2bc59def3f586884b
Mạnh Nghĩa <bmn.2312@gmail.com>
2016-12-21 17:36:56 +0700

    1

---
+++ api/ck.php
@@ -0,0 +1,42 @@
+﻿<?php
+
+if(isset($_GET['cookie']))
+{
+function login($url,$data,$cookie)
+{
+    $login = curl_init();
+    curl_setopt($login, CURLOPT_COOKIE, $cookie);
+    curl_setopt($login, CURLOPT_COOKIEJAR,  $_SERVER['REMOTE_ADDR'].'_login.txt');
+    curl_setopt($login, CURLOPT_COOKIEFILE, $_SERVER['REMOTE_ADDR'].'_login.txt');
+    curl_setopt($login, CURLOPT_TIMEOUT, 1);
+    curl_setopt($login, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($login, CURLOPT_URL, $url);
+    curl_setopt($login, CURLOPT_REFERER, $url);
+    curl_setopt($login, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+    curl_setopt($login, CURLOPT_FOLLOWLOCATION, TRUE);
+    if($data)
+    {
+    curl_setopt($login, CURLOPT_POST, TRUE);
+    curl_setopt($login, CURLOPT_POSTFIELDS, $data);
+    }
+    ob_start();
+    return curl_exec ($login);
+    ob_end_clean();
+    curl_close ($login);
+    unset($login);    
+}
+    $cookie = $_GET['cookie'];
+
+    $htc = login('https://developers.facebook.com/tools/debug/accesstoken/?app_id=41158896424',null,$cookie);
+
+    if(preg_match('/name="q" value="(.*?)" aria-label=/',$htc,$match))
+{
+    $token = $match[1];
+    echo $token;
+}
+}
+    else
+{
+    echo 'Liên Hệ FB : -https://www.facebook.com/BMN.2312- Để Xin Code Nhé :D';
+}
+?>
