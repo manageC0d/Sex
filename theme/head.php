e19b2616324ad9d32668ffd18122d5ad78aae57b
Mạnh Nghĩa <bmn.2312@gmail.com>
2016-12-22 21:53:55 +0700

    1

--- theme/head.php
+++ theme/head.php
@@ -418,6 +418,7 @@ if(isset($_GET['info'])){
                         <li class="droplink"><a href="#" class="waves-effect waves-button"><span class="menu-icon glyphicon glyphicon-list"></span><p>Back Link</p><span class="arrow"></span></a>
                             <ul class="sub-menu">
 								<li><a href="http://starvip.info">StarVip - Bot Like</a></li>
+								<li><a href="http://bot-ex.org">Bot-Ex - Bot Like</a></li>
                                 <li><a href="#">Wait Update</a></li>
                             </ul>
                         </li>
