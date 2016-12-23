b678900b5159abb16368c617047c5d57f0e02377
Mạnh Nghĩa <bmn.2312@gmail.com>
2016-12-24 00:30:55 +0700

    1

---
+++ tienich/liveFB/connect.facebook.net/en_US/sdk.js
@@ -0,0 +1,164 @@
+﻿<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" lang="en"/>
+    <title>LiveVideoFB.Com - Live Video FB on PC</title>
+    <meta name="description" content="LiveVideoFB.COM - Live Video Facebook on your computer without iPhone, you can live video every thing you want">
+<meta name="keyword" content="live video,livefb,without iphone,livevideofb.com">
+<img hidden alt="LiveVideoFB.COM - Live Video Facebook on your computer without iPhone, you can live video every thing you want">
+<meta name="author" content="Thanh Bình">
+<meta property="og:url" content="https://livevideofb.com">
+<meta property="og:type" content="article">
+<meta property="og:title" content="LiveVideoFB.COM - Live Video Facebook on your computer without iPhone, you can live video every thing you want">
+<meta property="og:description" content="LiveVideoFB.COM - Live Video Facebook on your computer without iPhone, you can live video every thing you want">
+<meta property="og:image" content="images/fbshare.png">
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link rel="icon" href="images/favicon.png">
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-77805514-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<style>.center{text-align: center;}</style>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1814882332081485',
+      xfbml      : true,
+      version    : 'v2.6'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+</head>
+<body>
+<div class="container">
+	<div class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <a href="../" class="navbar-brand">LiveVideoFB.Com</a>
+          <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div class="navbar-collapse collapse" id="navbar-main">
+          <ul class="nav navbar-nav">
+            
+            <li>
+              <a href="/blog">Blog</a>
+            </li>
+            <li>
+              <a href="#">Fix Error</a>
+            </li>
+            <li class="dropdown">
+              <a class="dropdown-toggle" data-toggle="dropdown" href="#" id="themes" >Contact Support<span class="caret"></span></a>
+              <ul class="dropdown-menu" aria-labelledby="themes">
+                <li><a href="https://www.facebook.com/thanhbinh.bm" target="_blank ">Thanh Bình Facebook</a></li>
+
+              </ul>
+            </li>
+
+          </ul>
+
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="/blog/guide/live-video-facebook-computer-iphone/" target="_blank">How to use ?</a></li>
+           <li><a href="#" target="_blank">Donate</a></li>
+		   <li><a href="http://livefb.vn" target="_blank">Tiếng Việt</a></li>
+          </ul>
+
+        </div>
+      </div>
+    </div> <!-- Ket thuc Nav -->
+	</div>
+	
+    <!--Logo-->
+    <div class="container">
+	<div class="page-header" id="banner">
+
+<div class="row"><li class="divider"></li></div>
+<div class="row"><div class="alert alert-dismissible alert-warning">
+  <button type="button" class="close" data-dismiss="alert">&times;</button>
+  <h4>Warning!</h4>
+  <p>UNINSTALL Adblock, because if you install it "Go Live" button will not work</p>
+</div></div></div>
+<!-- <div class="alert alert-dismissible alert-success">
+<style>.video-container {
+    position: relative;
+    padding-bottom: 56.25%;
+    padding-top: 30px; height: 0; overflow: hidden;
+}
+ 
+.video-container iframe,
+.video-container object,
+.video-container embed {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}</style>
+<button type="button" class="close" data-dismiss="alert">&times;</button>
+  <div class="center"><h4>Video</h4>
+<div class="video-container"><iframe width="700" height="400" src="https://www.youtube.com/embed/Q2lcl5-HczA?autoplay=1" frameborder="0" allowfullscreen></iframe></div></div>
+</div> -->	<div class="center">
+          
+<div class="page-header" id="banner">
+          <img src="images/logo.png"height="210" width="300" >
+            <h1>LiveVideoFB.Com</h1>
+            <p class="lead">Live Video Facebook on PC</p>
+          </div></div>
+		  
+<div class="container">
+<a id="livevideo"class="btn btn-primary btn-lg btn-block">Create Live Video</a>
+</a>
+<script>
+<!--
+document.write(unescape("%3Cscript%3E%0Adocument.getElementById%28%27livevideo%27%29.onclick%20%3D%20function%28%29%20%7B%0A%20%20FB.ui%28%7B%0A%20%20%20%20display%3A%20%27popup%27%2C%0A%20%20%20%20method%3A%20%27live_broadcast%27%2C%0A%20%20%20%20phase%3A%20%27create%27%2C%0A%7D%2C%20function%28response%29%20%7B%0A%20%20%20%20if%20%28%21response.id%29%20%7B%0A%20%20%20%20%20%20alert%28%27B%u1EA1n%20%u0111%E3%20hu%u1EF7%20stream%27%29%3B%0A%20%20%20%20%20%20return%3B%0A%20%20%20%20%7D%0A%20%20%20%20/*alert%28%27stream%20url%3A%27%20+%20response.secure_stream_url%29%3B*/%0A%20%20%20%20FB.ui%28%7B%0A%20%20%20%20%20%20display%3A%20%27popup%27%2C%0A%20%20%20%20%20%20method%3A%20%27live_broadcast%27%2C%0A%20%20%20%20%20%20phase%3A%20%27publish%27%2C%0A%20%20%20%20%20%20broadcast_data%3A%20response%2C%0A%20%20%20%20%7D%2C%20function%28response%29%20%7B%0A%20%20%20%20alert%28%22Tr%u1EA1ng%20th%E1i%20stream%3A%22%20+%20response.status%29%3B%0A%20%20%20%20%7D%29%3B%0A%20%20%7D%29%3B%0A%7D%3B%0A%3C/script%3E"));
+//-->
+</script>
+<div class="row"></div>
+<div class="center">
+<h2>OSB Download</h2>
+<div class="btn-group btn-group-justified">
+  <a href="https://github.com/jp9000/obs-studio/releases/download/0.14.2/OBS-Studio-0.14.2-Installer.exe" class="btn btn-info btn-default">For Windows</a>
+  <a href="https://github.com/jp9000/obs-studio/releases/download/0.14.2/obs-0.14.2-installer.dmg" class="btn btn-info btn-default">For Mac OS X</a>
+  <a href="https://obsproject.com/download#linux" class="btn btn-info btn-default">For Linux</a>
+</div>
+</div>
+</div>
+<!-- Het container -->
+<!-- Chay Javascript-->
+
+   <footer>
+        <div class="row">
+          <div class="col-lg-12">
+
+            <ul class="list-unstyled">
+              <li class="pull-right"><a href="#top">Top</a></li>
+          
+            </ul>
+            <p>Desgin by <a href="https://www.facebook.com/thanhbinh.bm" rel="nofollow">Thanh Bình</a></p>
+            <p>Esports Website : <a href="https://idtesports.com">IDT Esports</a></p>
+
+          </div>
+        </div>
+
+      </footer>
+<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/custom.js"></script>
+</body>
+</html>
