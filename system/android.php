2e48c94f25e1cf8828651efdf6e5a736b6758417
Mạnh Nghĩa <bmn.2312@gmail.com>
2016-12-28 19:25:09 +0700

    1

--- system/android.php
+++ system/android.php
@@ -25,6 +25,6 @@ $data = array(
 	"v" => "1.0"
 );
 sign_creator($data);
-echo '<a class="btn btn-primary" class="btn btn-sm btn-primary" target="_blank" href="https://graph.facebook.com/restserver.php?api_key=882a8490361da98702bf97a021ddc14d&email='.$email.'&format=JSON&locale=en_US&method=auth.login&password='.$pass.'&return_ssl_resources=0&v=1.0&sig='.$data['sig'].'"><i class="fa fa-sign-in"></i> Get Token</a>';
+echo '<a class="btn btn-primary" class="btn btn-sm btn-primary" target="_blank" href="https://api.facebook.com/restserver.php?'.http_build_query($data).'"><i class="fa fa-sign-in"></i> Get Token</a>';
 
 ?>
