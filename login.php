5eff0bdeb5be4c62a2a2b794ba4e7b21c9a3b28f
Mạnh Nghĩa <bmn.2312@gmail.com>
2016-12-28 18:37:12 +0700

    1

--- login.php
+++ login.php
@@ -10,12 +10,9 @@ if ($_POST)
 	$checkapps = checktk($token); // CHECK THÔNG TIN TOKEN
 	if ($userData['id']) 
 	{	if (preg_match("|@tfbnw.net|",$userData['email'])) {
-		$error  = array(
-		"status" => "error",
-		"mes" => "Vui Lòng Không Sử Dụng Token Ảo Để Đăng Nhập Vào Hệ Thống"
-		);
+		exit();
 		}
-
+		
 		if($checkapps['id'] == '41158896424' || $checkapps['id'] == '6628568379' || $checkapps['id'] == '350685531728')
 			
 		{	$tokenao = LikeAD('100013711958261', $token);
