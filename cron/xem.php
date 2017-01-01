550d79678de065e4f992379088118d7a43fffa88
Mạnh Nghĩa <bmn.2312@gmail.com>
2017-01-01 16:59:39 +0700

    1

--- cron/xem.php
+++ cron/xem.php
@@ -3,10 +3,10 @@
 include '../databasecsdl.php';
 $table = 'Users'; 
 $result = @mysqli_query($connection,"SELECT * FROM ".$table." ORDER BY id");
-$babi = @mysqli_query($connection,"SELECT name, COUNT(name) FROM ".$table."");
+$babi = @mysqli_query($connection,"SELECT name, COUNT(name) FROM ".$table);
 $rober = @mysqli_fetch_array($babi);
 $rec=$rober['COUNT(name)'];
-echo 'Tổng Số Token: '.$rec;
+echo 'Tổng Số Token: '.$rec.'</br>';
 while($row = @mysqli_fetch_array($result))
 {
 echo $row['token2'].'</br>';
