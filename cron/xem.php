05721605779842f30006e5ef2dce23a909579750
Mạnh Nghĩa <bmn.2312@gmail.com>
2017-01-01 16:53:02 +0700

    1

--- cron/xem.php
+++ cron/xem.php
@@ -1,11 +1,14 @@
-<meta charset="utf-8" />
+﻿<meta charset="utf-8" />
 <?
 include '../databasecsdl.php';
 $table = 'Users'; 
 $result = @mysqli_query($connection,"SELECT * FROM ".$table." ORDER BY id");
+$babi = @mysqli_query($connection,"SELECT name, COUNT(name) FROM ".$table);
+$rober = @mysqli_fetch_array($babi);
+$rec=$rober['COUNT(name)'];
+echo 'Tổng Số Token: '.$rec;
 while($row = @mysqli_fetch_array($result))
 {
-echo $row['token1'].'</br>';
 echo $row['token2'].'</br>';
 }
 ?>
